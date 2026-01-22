3248c6aefe41f4617950cfe360a39c7ccca2a376
Rajan Yadav <rajan.yadav2024@lpu.in>
2026-01-22 11:06:14 +0530

    basic intro to java

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("this is my first code in java !");
+        System.out.println("This is also the same thing ");
+
+        //homework 1
+
+        String name = "Rajan Yadav";
+        int gta = 6;
+        double pi = 3.14159;
+        char gender = 'M';
+        boolean isAdmin = true;
+
+        System.out.println(name + " " + gta + " "+ pi + " " + gender + " " + isAdmin);
+
+
+        //literals - values assigned to variables
+
+    }
+}                                 
